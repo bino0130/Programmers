1182b3d86da3ffd551b9ef7f4b47779fae082d69
bino <120705753+bino0130@users.noreply.github.com>
2023-07-30 18:24:50 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "Odd";
+        if(num % 2 == 0) answer = "Even";
+        else answer = answer;
+        return answer;
+    }
+}
