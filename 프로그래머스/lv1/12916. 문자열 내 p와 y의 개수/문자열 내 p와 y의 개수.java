33b8995dd91f7c696da4bd413da76727840998c4
bino <120705753+bino0130@users.noreply.github.com>
2023-07-30 20:14:41 +0900

    Update 문자열 내 p와 y의 개수.java

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,18 +1,14 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        String str = s.toLowerCase();
-        int p = 0, y = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'p') p++;
-            if (str.charAt(i) == 'y') y++;
+        int cntP = 0;
+        int cntY = 0;
+        String[] arr = s.toLowerCase().split("");
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].equals("p")) cntP++;
+            else if(arr[i].equals("y")) cntY++;
         }
-
-        if (p != y) answer = false;
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        //System.out.println("Hello Java");
-
+        if(cntP != cntY) answer = false;
         return answer;
     }
 }
