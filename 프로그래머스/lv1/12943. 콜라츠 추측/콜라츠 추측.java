e7ff2ae2d4ce4472a1c0f689caf565a34ef671de
bino <120705753+bino0130@users.noreply.github.com>
2023-07-31 08:42:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,19 +1,14 @@
 class Solution {
     public static int solution(long num) {
-        int cnt = 1;
-        return Collatz(num, 1);
+        if(num == 1) return 0;
+        return Collatz(num, 0);
     }
     
     public static int Collatz(long num, int cnt) {
-    	if(num == 1) return 0;
-    	long a = 0;
-        if(num % 2 == 0) a = num / 2;
-        else a = num * 3 + 1;
-        
-        if (a != 1) {
-            if((cnt+1) == 500) return -1;
-            else cnt++;
-            return Collatz(a, cnt);
-        } else return cnt;
+        if (num != 1 && (cnt+1) == 500) return -1;
+        if(num == 1) return cnt;  
+        if(num % 2 == 0) num = num / 2;
+        else num = num * 3 + 1;  
+        return Collatz(num, cnt + 1);
     }
 }
