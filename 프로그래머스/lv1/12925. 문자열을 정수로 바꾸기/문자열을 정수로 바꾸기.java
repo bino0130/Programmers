fe2e7f376be889878ad7dba1b738a2a9b284e553
bino <120705753+bino0130@users.noreply.github.com>
2023-07-30 20:46:04 +0900

    Delete 문자열을 정수로 바꾸기.java

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        if (s.contains("+")) answer = Integer.parseInt(s.replace("+", ""));
-        else answer = Integer.parseInt(s);
-        return answer;
-    }
-}
