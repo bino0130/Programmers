acde01c934b7cb60337f5bb6f7de29041da9e800
bino <120705753+bino0130@users.noreply.github.com>
2023-10-02 22:51:14 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 4.63 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        String a = myString.toLowerCase();
+        String b = pat.toLowerCase();
+        if(a.contains(b)) {
+            return 1;
+        } else return 0;
+    }
+}
